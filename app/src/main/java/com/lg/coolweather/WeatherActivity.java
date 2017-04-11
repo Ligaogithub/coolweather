49ef984ac0434ee84938e2790f03533145567b20
LiGao <ligaowangyi@163.com>
2017-04-11 16:26:12 +0800

    修改程序图标和名称。

--- app/src/main/java/com/lg/coolweather/WeatherActivity.java
+++ app/src/main/java/com/lg/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.lg.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.lg.coolweather.gson.Forecast;
 import com.lg.coolweather.gson.Weather;
+import com.lg.coolweather.service.AutoUpdateService;
 import com.lg.coolweather.util.HttpUtil;
 import com.lg.coolweather.util.Utility;
 
@@ -185,6 +187,11 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        if (weather != null && "ok".equals(weather.status)){
+            Intent intent = new Intent(this, AutoUpdateService.class);
+            startService(intent);
+        }
     }
 
     private void loadBingPic(){
